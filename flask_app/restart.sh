8f264d0030229f23e460e0a0a7c6f1f5951d3d90
Kaan Han Günay <kaan.gunay@epias.com.tr>
2023-05-23 22:40:38 +0300

    Restart script eklendi

---
+++ flask_app/restart.sh
@@ -0,0 +1,11 @@
+sudo chmod o+w /var/run
+sudo service syslog-ng start
+sudo service postgresql start
+sudo service slapd start
+sudo service ssh start
+sudo service nagios start
+sudo service apache2 start
+gunicorn --daemon --workers 1 --bind unix:/var/run/gunicorn.sock -m 007 app:app
+sudo chown www-data:www-data /var/run/gunicorn.sock
+sudo chmod 660 /var/run/gunicorn.sock
+sudo service nginx start
